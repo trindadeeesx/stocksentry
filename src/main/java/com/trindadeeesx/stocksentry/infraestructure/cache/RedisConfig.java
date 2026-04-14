3ed3b0f608266face0b13a60ee007276072f051e
trindadeeesx <trindadesx@proton.me>
2026-04-14 20:53:40 -0300

    feat(cache): add redis configuration for cache and faster requests

---
+++ src/main/java/com/trindadeeesx/stocksentry/infraestructure/cache/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.trindadeeesx.stocksentry.infraestructure.cache;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(5))
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair
+                                .fromSerializer(RedisSerializer.json()));
+
+        return RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .withCacheConfiguration("products",
+                        config.entryTtl(Duration.ofMinutes(5)))
+                .withCacheConfiguration("stock-summary",
+                        config.entryTtl(Duration.ofMinutes(2)))
+                .withCacheConfiguration("critical-products",
+                        config.entryTtl(Duration.ofMinutes(2)))
+                .build();
+    }
+}
