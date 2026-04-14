4b12423d7ccf2266cb526504c56790ad2005dc83
trindadeeesx <trindadesx@proton.me>
2026-04-14 00:20:41 -0300

    fix: enable async in application

--- src/main/java/com/trindadeeesx/stocksentry/StocksentryApplication.java
+++ src/main/java/com/trindadeeesx/stocksentry/StocksentryApplication.java
@@ -2,8 +2,10 @@ package com.trindadeeesx.stocksentry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class StocksentryApplication {
 
 	public static void main(String[] args) {
