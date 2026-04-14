f2a6a98756572a0625d969248a5cad6a811e8bda
trindadeeesx <trindadesx@proton.me>
2026-04-14 20:54:12 -0300

    fix(cache): enable caching and data web support

--- src/main/java/com/trindadeeesx/stocksentry/StocksentryApplication.java
+++ src/main/java/com/trindadeeesx/stocksentry/StocksentryApplication.java
@@ -2,10 +2,14 @@ package com.trindadeeesx.stocksentry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class StocksentryApplication {
 
 	public static void main(String[] args) {
