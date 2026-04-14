175d4f08d77963af15bc97c585512fdc15450ca9
trindadeeesx <trindadesx@proton.me>
2026-04-13 21:22:10 -0300

    chore: initial project structure with docker compose

---
+++ src/main/java/com/trindadeeesx/stocksentry/StocksentryApplication.java
@@ -0,0 +1,13 @@
+package com.trindadeeesx.stocksentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksentryApplication.class, args);
+	}
+
+}
